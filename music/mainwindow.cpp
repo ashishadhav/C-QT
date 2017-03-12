c8eb084f9c6e78d28cf7b09cf0a37218dac25b58
ashishadhav <aashish.aadhav@gmail.com>
2017-03-11 22:34:31 -0500

    started a simple application

---
+++ music/mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setText("WELCOME IN !!");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    exit(0);
+}
+
+void MainWindow::on_calendarWidget_clicked(const QDate &date)
+{
+    ui->label->setText(date.toString());
+    ui->listView->
+}
